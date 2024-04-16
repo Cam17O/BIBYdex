8be258cfe9842f7a8406548005671d3589f3b445
pauget camille <pauget.camille17@gmail.com>
2024-04-16 09:43:16 +0200

    docker-compose + page de base + connexion bdd

---
+++ site/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BIBYDEX</title>
+    <link rel="stylesheet" href="assets/css/index.css">
+</head>
+
+<body>
+
+    <!--Menu-->
+    <ul id="menu">
+        <li><a target="_blank" href="index.php" data-text="Accueil">Accueil</li>
+        <li><a target="_blank" href="#" data-text="Ma galerie">Ma galerie</li>
+        <li><a target="_blank" href="#" data-text="Liste des utilisateurs">Liste des utilisateurs</li>
+        <li><a target="_blank" href="login.php" data-text="Connexion">Se connecter</li>
+        <li><a target="_blank" href="CreateAccount.php" data-text="Créer un compte">Créer un compte</li>
+        <li><a target="_blank" href="#" data-text="Déconnexion">Se déconnecter</li>
+    </ul>
+    <!--Fin Menu-->
+
+
+</body>
+
+</html>
