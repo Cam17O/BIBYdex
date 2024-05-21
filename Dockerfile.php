9b93be055fec7ba82333d9aa4833c61fdf1b3af9
Camille Pauget <pauget.camille17@gmail.com>
2024-05-21 14:00:17 +0200

    route connexion

--- Dockerfile.php
+++ Dockerfile.php
@@ -4,6 +4,17 @@ FROM php:7.4-apache
 # Installation de l'extension PDO MySQL
 RUN docker-php-ext-install pdo_mysql
 
+# Installation de bcrypt
+RUN apt-get update && apt-get install -y \
+    libicu-dev \
+    && docker-php-ext-install intl \
+    && docker-php-ext-install bcmath \
+    && docker-php-ext-install opcache \
+    && docker-php-ext-install pcntl \
+    && docker-php-ext-install pdo_mysql \
+    && pecl install bcrypt \
+    && docker-php-ext-enable bcrypt
+
 # Créer un répertoire de travail dans l'image
 WORKDIR /var/www/html
 
